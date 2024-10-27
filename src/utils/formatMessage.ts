ea9b8b468522b2dd86a03028a9439f755ff2715e
Zane Rice <zanetrice@gmail.com>
2024-10-27 18:25:53 -0400

    Tests for errorHandler

--- src/utils/formatMessage.ts
+++ src/utils/formatMessage.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 function formatMessage(s: string, parameters: Record<string, any>) {
   return s.replace(/\{(\w+)\}/g, (match, key) => {
     return parameters[key] || match;
--- test/middlewares/errorHandler.test.ts
+++ test/middlewares/errorHandler.test.ts
@@ -1,5 +1,27 @@
-describe("bruh", () => {
-  it("should bruh", () => {
-    expect(true).toBeFalsy();
+import { Request, Response, NextFunction } from "express";
+import { errorHandler } from "../../src/middlewares";
+
+describe("errorHandler", () => {
+  const nextFunctionResult = "An error was passed to next function.";
+  const nextFunction = jest.fn().mockResolvedValue(nextFunctionResult);
+
+  it("should return result for a resolved promise", async () => {
+    const wrapper = errorHandler(jest.fn().mockResolvedValue(true));
+    const result = wrapper(
+      jest.fn() as unknown as Request,
+      jest.fn() as unknown as Response,
+      nextFunction
+    );
+    expect(await result).toBe(true);
+  });
+
+  it("should call next for a rejected promise", async () => {
+    const wrapper = errorHandler(jest.fn().mockRejectedValue(false));
+    const result = wrapper(
+      jest.fn() as unknown as Request,
+      jest.fn() as unknown as Response,
+      nextFunction
+    );
+    expect(await result).toEqual(nextFunctionResult);
   });
 });
