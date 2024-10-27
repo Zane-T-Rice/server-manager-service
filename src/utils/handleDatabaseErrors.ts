b3a488211d9bf38d7d5021b41817ef5c2bcf1ac0
Zane Rice <zanetrice@gmail.com>
2024-10-27 19:03:12 -0400

    handleDatabaseErrors tests

--- src/utils/handleDatabaseErrors.ts
+++ src/utils/handleDatabaseErrors.ts
@@ -4,7 +4,7 @@ import { Prisma } from "@prisma/client";
 function handleDatabaseErrors(err: Error, resource: string, ids: string[]) {
   if (err instanceof Prisma.PrismaClientKnownRequestError) {
     if (err.code === "P2025") {
-      throw new NotFoundError(resource, [ids[0]]);
+      throw new NotFoundError(resource, ids);
     }
   }
   throw new InternalServerError();
--- test/utils/handleDatabaseErrors.test.ts
+++ test/utils/handleDatabaseErrors.test.ts
@@ -1,5 +1,38 @@
-describe("bruh", () => {
-  it("should bruh", () => {
-    expect(true).toBeFalsy();
+import { handleDatabaseErrors } from "../../src/utils";
+import { InternalServerError, NotFoundError } from "../../src/errors";
+import { Prisma } from "@prisma/client";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+
+describe("handleDatabaseErrors", () => {
+  const resource = "server";
+  const ids = ["id"];
+
+  it("throws NotFoundError for Prisma code P2025", () => {
+    expect(() =>
+      handleDatabaseErrors(
+        new PrismaClientKnownRequestError("Server Not Found", {
+          code: "P2025",
+          clientVersion: "version",
+        }),
+        resource,
+        ids
+      )
+    ).toThrow(new NotFoundError(resource, ids));
+  });
+
+  it("throws InternalServerError for other Prisma codes", () => {
+    expect(() =>
+      handleDatabaseErrors(
+        { code: "P2037" } as Prisma.PrismaClientKnownRequestError,
+        resource,
+        ids
+      )
+    ).toThrow(new InternalServerError());
+  });
+
+  it("throws InternalServerError for errors that are not Prisma errors", async () => {
+    expect(() => handleDatabaseErrors(new Error(), resource, ids)).toThrow(
+      new InternalServerError()
+    );
   });
 });
