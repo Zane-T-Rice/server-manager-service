dffed1fb1db807f19441f20e99b3a54e30bed40b
Zane Rice <zanetrice@gmail.com>
2024-11-01 05:10:09 -0400

    Moving redocly to pre-commit because that makes way more sense.

---
+++ .githooks/pre-commit/1-redocly.sh
@@ -0,0 +1,5 @@
+(
+  cd swagger
+  redocly bundle serverManagerService.yaml > combinedServerManagerService.yaml
+  git add combinedServerManagerService.yaml
+)
--- .githooks/pre-push/0-nostaged.sh
+++
@@ -1,6 +0,0 @@
-if [ "$(git diff --name-only --cached)" != "" ]; then 
-  echo "Please do not push with files staged for commit."
-  echo "You can probably push using \"git stash --staged;git push;git stash pop\"."
-  echo "Just be warned that those commands will move the staged files back to unstaged, which could merge them with local, unstaged changes that you have."
-  exit 1;
-fi
