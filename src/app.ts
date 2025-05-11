4e78cd82353cc47e582ed1c0853479965207885d
Zane Rice <zanetrice@gmail.com>
2025-05-11 15:12:40 -0400

    /stop action requires server affinity, so adding it to that list

--- src/app.ts
+++ src/app.ts
@@ -117,12 +117,12 @@ app.use(
 // For paths beginning with Routes.PROXY, this host must be the correct host
 // or an error will be thrown. This prevents infinite proxy recursion.
 app.use(
-  `(${Routes.PROXY})?/hosts/:hostId/servers/:serverId/(update|restart)`,
+  `(${Routes.PROXY})?/hosts/:hostId/servers/:serverId/(update|restart|stop)`,
   requiredScopes(Permissions.ADMIN),
   errorHandler(proxyMiddleware(prisma))
 );
 app.use(
-  `(${Routes.PROXY})?/users/servers/:serverId/(update|restart)`,
+  `(${Routes.PROXY})?/users/servers/:serverId/(update|restart|stop)`,
   requiredScopes(Permissions.USER),
   errorHandler(proxyMiddleware(prisma))
 );
