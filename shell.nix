699746c6366b6a8cff9df35c5b8abe92c71db27f
Zane-T-Rice <zanetrice@gmail.com>
2025-11-02 02:33:21 -0500

    nix-shell update to always use verison 5.22 of prisma/prisma-engine

--- shell.nix
+++ shell.nix
@@ -1,4 +1,6 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import (builtins.fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/59133ee770406f605d61698bc4f1a89efcf461d5.tar.gz";
+    }) {} }:
 pkgs.mkShellNoCC {
  PRISMA_QUERY_ENGINE_LIBRARY = "${pkgs.prisma-engines}/lib/libquery_engine.node";
  PRISMA_QUERY_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/query-engine";
